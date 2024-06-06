ff86067cf31209e72b245d2cd07843aea610b0c9
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-06 15:04:39 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Home = () => {
   return (
     <>
-      <div className="font-black text-5xl text-center mt-10">Beny Bad Boy</div>
+      <div className="font-black text-5xl text-center mt-6">Beny Bad Boy</div>
       <Image
         src={"/bbb.jpg"}
         alt=""
@@ -14,7 +14,7 @@ const Home = () => {
         className="text-center m-auto"
       />
       <Link
-        className="btn btn-primary w-auto"
+        className="btn btn-primary w-auto m-auto mt-4"
         href={
           "https://xdc.sale/presale/0x6182c5cC8D21a63708e567684F6A01b691f24a5e"
         }
