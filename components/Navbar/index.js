deaa10874a3889c6c7d32090ad91c695c83692fd
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-07 19:55:45 +0400

    update

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,25 +1,37 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 const Navbar = () => {
   const router = useRouter();
   const pathname = router.pathname;
+  console.log(pathname);
+  const isHome = pathname === "/";
   return (
     <div className="navbar items-center h-10">
       <div className="navbar-start"></div>
       <div className="navbar-center flex"></div>
       <div className="navbar-end gap-2">
-        <Link href={"https://linktr.ee/benybadboy"} className="btn btn-accent">
-          Contact
-        </Link>
-        <Link
-          className="btn btn-primary"
-          href={
-            "https://xdc.sale/presale/0x6182c5cC8D21a63708e567684F6A01b691f24a5e"
-          }
-        >
-          IDO NOW!
-        </Link>
+        {isHome && (
+          <>
+            <Link
+              href={"https://linktr.ee/benybadboy"}
+              className="btn btn-accent"
+            >
+              Contact
+            </Link>
+            <Link
+              className="btn btn-primary"
+              href={
+                "https://xdc.sale/presale/0x6182c5cC8D21a63708e567684F6A01b691f24a5e"
+              }
+            >
+              IDO NOW!
+            </Link>
+          </>
+        )}
+        {!isHome && <ConnectButton />}
       </div>
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,35 @@ import { themeChange } from "theme-change";
 import React from "react";
 import Layout from "../components/Layout";
 
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { xdc } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "2a612b9a18e81ce3fda2f82787eb6a4a",
+  chains: [xdc],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
 const MyApp = ({ Component, pageProps }) => {
+  const { locale } = useRouter();
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider coolMode locale={locale}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
