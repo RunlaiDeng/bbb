1a6a7189db42123334c603a3452bfca949e9fd7a
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-07 10:41:41 +0400

    Update index.js

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -6,12 +6,14 @@ const Navbar = () => {
   const pathname = router.pathname;
   return (
     <div className="navbar items-center h-20">
-      <div className="navbar-start">
-      </div>
+      <div className="navbar-start"></div>
       <div className="navbar-center flex"></div>
-      <div className="navbar-end">
+      <div className="navbar-end gap-2">
+        <Link href={"https://linktr.ee/benybadboy"} className="btn btn-accent">
+          Linktree
+        </Link>
         <Link
-          className="btn btn-primary w-auto m-auto mt-4"
+          className="btn btn-primary"
           href={
             "https://xdc.sale/presale/0x6182c5cC8D21a63708e567684F6A01b691f24a5e"
           }
