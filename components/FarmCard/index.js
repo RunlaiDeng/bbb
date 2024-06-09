f650c7eab8c21baf467f938468f04aa2160c9aa9
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-06-09 20:07:41 +0400

    Update index.js

--- components/FarmCard/index.js
+++ components/FarmCard/index.js
@@ -207,8 +207,7 @@ const FarmCard = () => {
               <Link
                 className="underline"
                 href={
-                  "https://app.xspswap.finance/swap#/swap?outputCurrency=" +
-                  bbb?.address
+                  "https://app.xspswap.finance/swap#/swap?outputCurrency=0xFa4dDcFa8E3d0475f544d0de469277CF6e0A6Fd1"
                 }
                 target="_blank"
               >
