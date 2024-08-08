10883d81354071bc686f9b93500953a16801b794
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-08-08 13:40:20 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -186,7 +186,7 @@ const Home = () => {
             {dropTokens?.map((item, index) => {
               return (
                 <div
-                  className="card card-side shadow-xl cursor-pointer"
+                  className="card card-side shadow-xl cursor-pointer hover:border-4 border-violet-500"
                   key={index}
                   onClick={() => {
                     window.open(
