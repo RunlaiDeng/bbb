0b237f63661a7ded9ab1c4105888f5ccdba507fe
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-09-22 12:10:58 +0400

    Update index.js

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -72,36 +72,48 @@ const Navbar = () => {
       </div>
       <div className="navbar-center hidden xl:flex"></div>
       <div className="navbar-end">
-        <ConnectButton
-          accountStatus={{
-            smallScreen: "avatar",
-            largeScreen: "full",
-          }}
-          chainStatus="icon"
-          showBalance="false"
-        />
-
-        <div className="dropdown dropdown-end mx-1 md:hidden">
-          <div tabIndex={0} role="button" className="btn m-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
+        <div className="hidden md:flex">
+          <ConnectButton
+            accountStatus={{
+              smallScreen: "avatar",
+              largeScreen: "full",
+            }}
+            chainStatus="icon"
+            showBalance="false"
+          />
+        </div>
+        <div className="drawer drawer-end md:hidden">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <label
+              htmlFor="my-drawer"
+              className="btn drawer-button float-right"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-5 w-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
           </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow font-black"
-          >
-            <ul className="menu rounded-box">
+          <div className="drawer-side z-50">
+            <label
+              htmlFor="my-drawer"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+              {/* Sidebar content here */}
+
               <li>
                 <Link
                   className="btn mx-1 btn-ghost hover:bg-inherit animate-shake border-2 animate-shake-border"
@@ -142,8 +154,20 @@ const Navbar = () => {
                   </li>
                 </ul>
               </li>
+              <li>
+                <div>
+                  <ConnectButton
+                    accountStatus={{
+                      smallScreen: "avatar",
+                      largeScreen: "full",
+                    }}
+                    chainStatus="icon"
+                    showBalance="false"
+                  />
+                </div>
+              </li>
             </ul>
-          </ul>
+          </div>
         </div>
       </div>
     </div>
