b20b87ba6dcde59e9483bd45934de0918d42a020
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-10-08 09:59:48 +0400

    Update v2.js

--- pages/v2.js
+++ pages/v2.js
@@ -19,6 +19,7 @@ import { parseEther, formatEther } from "viem";
 
 const getDate = (timestamp) => {
   
+
   const date = new Date(timestamp * 1000);
 
   const day = String(date.getDate()).padStart(2, "0");
