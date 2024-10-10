0497638bd82d4c66c0ee8dca6beb953bdd0594f5
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-10-10 14:09:36 +0400

    update

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="bg-[url('/bg.png')] bg-center w-full">
+      <div className="bg-[url('/bg.png')] bg-center w-full h-16">
         <div className="navbar items-center font-black">
           <div className="navbar-start">
             <Image
