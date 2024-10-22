875a1a7a0fd13f87203e57b275d961b6fc9b9898
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-10-22 19:50:01 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "../styles/globals.css";
 import React from "react";
 import Layout from "../components/Layout";
 import "@rainbow-me/rainbowkit/styles.css";
-import { http } from "wagmi";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { WagmiProvider } from "wagmi";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
@@ -45,7 +44,6 @@ const xdcParentNet = {
     symbol: "XDC",
   },
   rpcUrls: {
-    public: { http: ["https://devnetstats.apothem.network/devnet"] },
     default: { http: ["https://devnetstats.apothem.network/devnet"] },
   },
   iconUrl: "/bbb.jpg",
@@ -57,10 +55,6 @@ const config = getDefaultConfig({
   appName: "Benybadboy",
   projectId: "2a612b9a18e81ce3fda2f82787eb6a4a",
   chains: [xdc, xdcParentNet],
-  transports: {
-    [xdc.id]: http("https://rpc.xdcrpc.com"),
-    [xdcParentNet.id]: http("https://devnetstats.apothem.network/devnet"),
-  },
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
