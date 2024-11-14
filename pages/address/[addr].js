d6339324a51d69e36f0ad0db3cfa7c0cdb5dadb6
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-11-14 22:22:42 +0400

    Update [addr].js

--- pages/address/[addr].js
+++ pages/address/[addr].js
@@ -1,6 +1,8 @@
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const Address = () => {
+  const router = useRouter();
   const { addr } = router.query;
 
   const [mount, setMount] = useState(false);
