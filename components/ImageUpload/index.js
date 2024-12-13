290d7c84d0ff8fc584cfcedd7a975ed03b1ca169
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-12-13 20:39:35 +0400

    Update index.js

--- components/ImageUpload/index.js
+++ components/ImageUpload/index.js
@@ -75,25 +75,9 @@ export default function ImageUpload(props) {
       >
         {loading && ( // Show loading indicator
           <div className="flex flex-col items-center">
-            <svg
-              className="animate-spin w-12 h-12 text-green-700 mb-4" // Add spin animation
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                fill="none"
-                strokeWidth="4"
-              />
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 1 1 16 0A8 8 0 0 1 4 12z"
-              />
-            </svg>
+            <div className="flex justify-center items-center">
+              <div className="loading loading-bars loading-lg text-success"></div>
+            </div>
             <p className="text-xs text-gray-400">Uploading...</p>
           </div>
         )}
