d54dc77ece63757f219008e9acb53822745eca13
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-12-13 16:14:05 +0400

    update

--- components/TokenMarkets/index.js
+++ components/TokenMarkets/index.js
@@ -1,40 +1,48 @@
 import { useEffect, useState } from "react";
 import rpc from "@/components/Rpc";
 import { useRouter } from "next/router";
-import { getPool } from "../Utils";
-import { bbbInfo } from "@/config";
+import { getPool, handleSrc } from "../Utils";
+import { bbbInfo, contracts } from "@/config";
 import { useFollow } from "../Context/follow";
-import Loading from "../Loading";
+import { useChainId, useReadContracts } from "wagmi";
+import Image from "next/image";
 
 const TokenMarkets = (props) => {
-  const { xdcPrice, xdcPriceChangeH24 } = props;
+  const {
+    xdcPrice,
+    xdcPriceChangeH24,
+    searchBar = true,
+    showBar = true,
+    pageSize = 99999,
+  } = props;
+
   const [data, setData] = useState({});
   const [show, setShow] = useState(2);
 
   const { follow, setFollow } = useFollow();
-  useEffect(() => {
-    async function fetchData() {
-      const bbbPool = await getPool(bbbInfo.address);
+  async function fetchData() {
+    const bbbPool = await getPool(bbbInfo.address);
 
-      const bbb = {
-        price: Number(bbbPool?.base_token_price_usd || 0)?.toFixed(4),
-        changeH24: bbbPool?.price_change_percentage?.h24 || 0,
-      };
-      let findTokens = {};
-      if (show == 1) {
-        const followList = Object.keys(follow).filter((key) => follow[key]);
-        if (followList?.length > 0) {
-          findTokens = await rpc.getTokens(1, 1, 9999, followList);
-        }
+    const bbb = {
+      price: Number(bbbPool?.base_token_price_usd || 0)?.toFixed(4),
+      changeH24: bbbPool?.price_change_percentage?.h24 || 0,
+    };
+    let findTokens = {};
+    if (show == 1) {
+      const followList = Object.keys(follow).filter((key) => follow[key]);
+      if (followList?.length > 0) {
+        findTokens = await rpc.getTokens(1, 1, pageSize, followList);
       }
-      if (show == 2) {
-        findTokens = await rpc.getTokens(1, 1, 9999);
-      }
-
-      setData({ ...data, tokenList: findTokens?.list, bbb });
     }
+    if (show == 2) {
+      findTokens = await rpc.getTokens(1, 1, pageSize);
+    }
+
+    setData({ ...data, tokenList: findTokens?.list, bbb });
+  }
+  useEffect(() => {
     fetchData();
-  }, [show, follow]);
+  }, [show, follow, pageSize]);
   const tokenList = data?.tokenList;
 
   const router = useRouter();
@@ -43,97 +51,118 @@ const TokenMarkets = (props) => {
   const bbbPriceChange24h = data?.bbb?.changeH24;
 
   const showList = tokenList?.length > 0;
+  const chainId = useChainId();
 
+  const mbbb = contracts[chainId]?.mbbbv2;
   const showBBB = bbbPrice && bbbPriceChange24h;
+  let searchTokens = [];
+  if (!showBar) {
+    searchTokens = tokenList?.map((item) => {
+      return {
+        ...mbbb,
+        functionName: "getDropTokenByAddress",
+        args: [item?.token],
+      };
+    });
+  }
+  const { data: reads0 } = useReadContracts({
+    contracts: searchTokens,
+  });
+
+  const drops = reads0?.map((item) => item?.result);
 
   return (
     <>
       <div className="card">
         <div className="card-body p-0 h-[500px] lg:h-full">
-          <div className="p-2 flex items-center gap-2">
-            <label className=" w-full flex items-center input input-bordered gap-2 input-sm">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="h-4 w-4 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                  clipRule="evenodd"
+          {searchBar && (
+            <div className="p-2 flex items-center gap-2">
+              <label className=" w-full flex items-center input input-bordered gap-2 input-sm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="h-4 w-4 opacity-70"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  type="text"
+                  className="w-full"
+                  placeholder="0x or bbb"
+                  onChange={(e) => {
+                    setData({ ...data, search: e.target.value });
+                  }}
                 />
-              </svg>
-              <input
-                type="text"
-                className="w-full"
-                placeholder="0x or bbb"
-                onChange={(e) => {
-                  setData({ ...data, search: e.target.value });
-                }}
-              />
-            </label>
+              </label>
 
-            <div
-              className="btn btn-sm cursor-pointer"
-              onClick={() => {
-                if (data?.search) {
-                  router.push("/swap/" + data?.search);
-                } else {
-                  //   info("please submit token address");
-                }
-              }}
-            >
-              Search
-            </div>
-          </div>
-          <div className="flex gap-2 text-slate-500">
-            <div role="tablist" className="tabs tabs-bordered w-full">
-              <a
-                role="tab"
-                className={"tab " + (show == 1 ? "tab-active" : "")}
+              <div
+                className="btn btn-sm cursor-pointer"
                 onClick={() => {
-                  setShow(1);
+                  if (data?.search) {
+                    router.push("/swap/" + data?.search);
+                  } else {
+                    //   info("please submit token address");
+                  }
                 }}
               >
-                <svg
-                  viewBox="0 0 1024 1024"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  p-id="5267"
-                  width="16"
-                  height="16"
-                >
-                  <path
-                    d="M785.352203 933.397493c-4.074805 0-8.151657-0.970094-11.833513-3.007497l-261.311471-142.488225L250.942821 930.388972c-8.343015 4.559852-18.527982 3.8814-26.28669-1.599428-7.760754-5.5279-11.640108-14.987343-10.088776-24.347524l47.578622-285.365306L72.563154 429.470355c-6.594185-6.547113-8.971325-16.295128-6.110161-25.122167 2.814092-8.850575 10.379395-15.397688 19.546172-16.949021l285.512662-47.577598 118.529557-236.989529c4.172019-8.391111 12.803607-13.701047 22.165836-13.701047 9.359158 0 17.992793 5.309936 22.163789 13.701047l118.529557 236.989529 285.511639 47.577598c9.217942 1.551332 16.73208 8.051373 19.593244 16.949021 2.813069 8.875135 0.48607 18.575054-6.109138 25.122167L762.264369 619.077737l47.577598 285.365306c1.50119 9.360182-2.37714 18.819624-10.087753 24.347524C795.487028 931.797042 790.394033 933.397493 785.352203 933.397493z"
-                    p-id="5268"
-                    fill="#0e932e"
-                  ></path>
-                </svg>
-              </a>
-              <a
-                role="tab"
-                className={"tab " + (show == 2 ? "tab-active" : "")}
-                onClick={() => {
-                  setShow(2);
-                }}
-              >
-                <svg
-                  viewBox="0 0 1024 1024"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  p-id="6437"
-                  width="16"
-                  height="16"
-                >
-                  <path
-                    d="M370.857143 561.714286l-266.285714 266.285714q-5.714286 5.714286-13.142858 5.714286t-13.142857-5.714286l-28.571428-28.571429q-5.714286-5.714286-5.714286-13.142857t5.714286-13.142857l224.571428-224.571428L49.714286 324q-5.714286-5.714286-5.714286-13.142857t5.714286-13.142857l28.571428-28.571429q5.714286-5.714286 13.142857-5.714286t13.142858 5.714286l266.285714 266.285714q5.714286 5.714286 5.714286 13.142858t-5.714286 13.142857z m616.571428 261.142857v36.571428q0 8-5.142857 13.142858t-13.142857 5.142857H420.571429q-8 0-13.142858-5.142857t-5.142857-13.142858v-36.571428q0-8 5.142857-13.142857t13.142858-5.142857h548.571428q8 0 13.142857 5.142857t5.142857 13.142857z"
-                    p-id="6438"
-                  ></path>
-                </svg>
-              </a>
+                Search
+              </div>
             </div>
-          </div>
+          )}
+          {showBar && (
+            <div className="flex gap-2 text-slate-500">
+              <div role="tablist" className="tabs tabs-bordered w-full">
+                <a
+                  role="tab"
+                  className={"tab " + (show == 1 ? "tab-active" : "")}
+                  onClick={() => {
+                    setShow(1);
+                  }}
+                >
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    p-id="5267"
+                    width="16"
+                    height="16"
+                  >
+                    <path
+                      d="M785.352203 933.397493c-4.074805 0-8.151657-0.970094-11.833513-3.007497l-261.311471-142.488225L250.942821 930.388972c-8.343015 4.559852-18.527982 3.8814-26.28669-1.599428-7.760754-5.5279-11.640108-14.987343-10.088776-24.347524l47.578622-285.365306L72.563154 429.470355c-6.594185-6.547113-8.971325-16.295128-6.110161-25.122167 2.814092-8.850575 10.379395-15.397688 19.546172-16.949021l285.512662-47.577598 118.529557-236.989529c4.172019-8.391111 12.803607-13.701047 22.165836-13.701047 9.359158 0 17.992793 5.309936 22.163789 13.701047l118.529557 236.989529 285.511639 47.577598c9.217942 1.551332 16.73208 8.051373 19.593244 16.949021 2.813069 8.875135 0.48607 18.575054-6.109138 25.122167L762.264369 619.077737l47.577598 285.365306c1.50119 9.360182-2.37714 18.819624-10.087753 24.347524C795.487028 931.797042 790.394033 933.397493 785.352203 933.397493z"
+                      p-id="5268"
+                      fill="#0e932e"
+                    ></path>
+                  </svg>
+                </a>
+                <a
+                  role="tab"
+                  className={"tab " + (show == 2 ? "tab-active" : "")}
+                  onClick={() => {
+                    setShow(2);
+                  }}
+                >
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    p-id="6437"
+                    width="16"
+                    height="16"
+                  >
+                    <path
+                      d="M370.857143 561.714286l-266.285714 266.285714q-5.714286 5.714286-13.142858 5.714286t-13.142857-5.714286l-28.571428-28.571429q-5.714286-5.714286-5.714286-13.142857t5.714286-13.142857l224.571428-224.571428L49.714286 324q-5.714286-5.714286-5.714286-13.142857t5.714286-13.142857l28.571428-28.571429q5.714286-5.714286 13.142857-5.714286t13.142858 5.714286l266.285714 266.285714q5.714286 5.714286 5.714286 13.142858t-5.714286 13.142857z m616.571428 261.142857v36.571428q0 8-5.142857 13.142858t-13.142857 5.142857H420.571429q-8 0-13.142858-5.142857t-5.142857-13.142858v-36.571428q0-8 5.142857-13.142857t13.142858-5.142857h548.571428q8 0 13.142857 5.142857t5.142857 13.142857z"
+                      p-id="6438"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          )}
 
           <div className="overflow-auto ">
             <table className="table table-xs">
@@ -154,20 +183,34 @@ const TokenMarkets = (props) => {
                     }}
                   >
                     <td className="flex items-center gap-1">
-                      <svg
-                        viewBox="0 0 1024 1024"
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        p-id="5267"
-                        width="16"
-                        height="16"
-                      >
-                        <path
-                          d="M785.352203 933.397493c-4.074805 0-8.151657-0.970094-11.833513-3.007497l-261.311471-142.488225L250.942821 930.388972c-8.343015 4.559852-18.527982 3.8814-26.28669-1.599428-7.760754-5.5279-11.640108-14.987343-10.088776-24.347524l47.578622-285.365306L72.563154 429.470355c-6.594185-6.547113-8.971325-16.295128-6.110161-25.122167 2.814092-8.850575 10.379395-15.397688 19.546172-16.949021l285.512662-47.577598 118.529557-236.989529c4.172019-8.391111 12.803607-13.701047 22.165836-13.701047 9.359158 0 17.992793 5.309936 22.163789 13.701047l118.529557 236.989529 285.511639 47.577598c9.217942 1.551332 16.73208 8.051373 19.593244 16.949021 2.813069 8.875135 0.48607 18.575054-6.109138 25.122167L762.264369 619.077737l47.577598 285.365306c1.50119 9.360182-2.37714 18.819624-10.087753 24.347524C795.487028 931.797042 790.394033 933.397493 785.352203 933.397493z"
-                          p-id="5268"
-                          fill="#0e932e"
-                        ></path>
-                      </svg>
+                      {showBar ? (
+                        <svg
+                          viewBox="0 0 1024 1024"
+                          version="1.1"
+                          xmlns="http://www.w3.org/2000/svg"
+                          p-id="5267"
+                          width="16"
+                          height="16"
+                        >
+                          <path
+                            d="M785.352203 933.397493c-4.074805 0-8.151657-0.970094-11.833513-3.007497l-261.311471-142.488225L250.942821 930.388972c-8.343015 4.559852-18.527982 3.8814-26.28669-1.599428-7.760754-5.5279-11.640108-14.987343-10.088776-24.347524l47.578622-285.365306L72.563154 429.470355c-6.594185-6.547113-8.971325-16.295128-6.110161-25.122167 2.814092-8.850575 10.379395-15.397688 19.546172-16.949021l285.512662-47.577598 118.529557-236.989529c4.172019-8.391111 12.803607-13.701047 22.165836-13.701047 9.359158 0 17.992793 5.309936 22.163789 13.701047l118.529557 236.989529 285.511639 47.577598c9.217942 1.551332 16.73208 8.051373 19.593244 16.949021 2.813069 8.875135 0.48607 18.575054-6.109138 25.122167L762.264369 619.077737l47.577598 285.365306c1.50119 9.360182-2.37714 18.819624-10.087753 24.347524C795.487028 931.797042 790.394033 933.397493 785.352203 933.397493z"
+                            p-id="5268"
+                            fill="#0e932e"
+                          ></path>
+                        </svg>
+                      ) : (
+                        <div className="w-8 h-8 flex items-center justify-center overflow-hidden ">
+                          <Image
+                            height={400}
+                            width={400}
+                            src={"/bbb.jpg"}
+                            alt={""}
+                            className="object-cover w-full h-full"
+                            loading="lazy"
+                            priority={false}
+                          />
+                        </div>
+                      )}
                       BBB
                     </td>
                     <td className="text-right">${bbbPrice}</td>
@@ -207,7 +250,7 @@ const TokenMarkets = (props) => {
                         }}
                       >
                         <td className="flex items-center gap-1">
-                          {typeof window !== "undefined" && (
+                          {typeof window !== "undefined" && showBar && (
                             <label
                               className="swap"
                               onClick={(e) => {
@@ -258,6 +301,19 @@ const TokenMarkets = (props) => {
                                 </svg>
                               </div>
                             </label>
+                          )}
+                          {!showBar && (
+                            <div className="w-8 h-8 flex items-center justify-center overflow-hidden ">
+                              <Image
+                                height={400}
+                                width={400}
+                                src={handleSrc(drops?.[index]?.imageUrl)}
+                                alt={""}
+                                className="object-cover w-full h-full"
+                                loading="lazy"
+                                priority={false}
+                              />
+                            </div>
                           )}
                           {item?.symbol}
                         </td>
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,92 @@
 import usePrivyLogin from "@/components/Hook/usePrivyLogin";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { memo } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import TokenMarkets from "@/components/TokenMarkets";
+import { getXDCPrice } from "@/components/Utils";
 
 const HomeContent = memo(() => {
   const privyLogin = usePrivyLogin();
   const router = useRouter();
+  const [mount, setMount] = useState(false);
+  const { address } = useAccount();
 
-  const handleTryNow = async () => {
-    await privyLogin();
-    router.push("/markets");
+  const [price, setPrice] = useState({});
+
+  async function fetchData() {
+    setMount(false);
+    const xdc = await getXDCPrice();
+
+    setPrice({ ...price, xdc });
+    setMount(true);
+  }
+
+  const xdcPrice = price?.xdc?.price;
+
+  const xdcPriceChangeH24 = price?.xdc?.priceChange24h;
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleTryNow = useCallback(async () => {
+    try {
+      if (!address) {
+        await privyLogin();
+        router.push("/markets");
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  }, [privyLogin, router, address]);
+
+  const tMarkets = {
+    showBar: false,
+    searchBar: false,
+    pageSize: 4,
+    xdcPrice,
+    xdcPriceChangeH24,
+    showLogo: true,
   };
 
   return (
     <>
       <div className="card min-h-screen sm:w-3/4 m-auto">
         <div className="card-body">
-          <div className="sm:flex items-center gap-2 sm:mt-8">
+          <div className="sm:flex items-center gap-4 mt-24 sm:mt-48">
             <div className="text-center sm:text-left">
               <h1 className="text-green-700 font-bold sm:text-5xl">
-                Next generation exchange and all is on blockchain
+                {address
+                  ? "Discuss Everything Crypto On BBBPump.fun"
+                  : "Next generation exchange and all is on blockchain"}
               </h1>
-              <button
-                className="btn btn-success text-white sm:btn-lg mt-8 mx-4 w-72 sm:w-96 hover:bg-white hover:text-green-700 outline outline-2"
-                onClick={handleTryNow}
-                aria-label="Try Now"
-              >
-                Try Now
-              </button>
+              {!address && (
+                <button
+                  className="btn btn-success text-white sm:btn-lg mt-8 mx-4 w-72 sm:w-96 hover:bg-white hover:text-green-700 outline outline-2"
+                  onClick={handleTryNow}
+                  aria-label="Try Now"
+                  type="button"
+                >
+                  Sign Up
+                </button>
+              )}
+              {address && (
+                <button
+                  className="btn sm:btn-lg mt-8 mx-4 w-72 sm:w-96 text-green-700"
+                  onClick={() => {
+                    router.push("/swap/bbb");
+                  }}
+                  aria-label="Try Now"
+                  type="button"
+                >
+                  Trade
+                </button>
+              )}
               <div className="mt-8" />
             </div>
-            <div className="mt-10">
-              <div className="mockup-browser border-green-700 border border-4">
-                <Image
-                  src="/home0.png"
-                  height={1000}
-                  width={1000}
-                  className="w-full"
-                  alt="Platform Preview"
-                  priority
-                />
-              </div>
+            <div>
+              <TokenMarkets {...tMarkets} />
             </div>
           </div>
           <div className="m-auto my-10 sm:mt-0" />
@@ -63,6 +109,7 @@ const HomeContent = memo(() => {
               className="w-full"
               alt="Trading Interface"
               loading="lazy"
+              quality={75}
             />
           </div>
         </div>
@@ -73,5 +120,7 @@ const HomeContent = memo(() => {
 
 HomeContent.displayName = "HomeContent";
 
-const Home = () => <HomeContent />;
+const Home = memo(() => <HomeContent />);
+Home.displayName = "Home";
+
 export default Home;
