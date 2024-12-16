4a46c5f0b20cae5329c6818b18dd58f27569a5f1
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2024-12-16 16:04:12 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import Layout from "../components/Layout";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
@@ -13,6 +13,7 @@ import Head from "next/head";
 import { PrivyProvider } from "@privy-io/react-auth";
 import { FollowProvider } from "@/components/Context/follow";
 import { xdc } from "../config/chains";
+import Script from "next/script";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -57,19 +58,29 @@ const privyConfig = {
 
 const MyApp = ({ Component, pageProps }) => {
   const { locale } = useRouter();
-  
+
   return (
     <>
       <Head>
         <title>BBBPump | The First Meme Launch Platform on XDC Network</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-FLNW2BCHW0"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FLNW2BCHW0');
+        `}
+      </Script>
       <NotificationProvider>
         <FollowProvider>
-          <PrivyProvider
-            appId="cm3uezqn203md3kangodaq4u6"
-            config={privyConfig}
-          >
+          <PrivyProvider appId="cm3uezqn203md3kangodaq4u6" config={privyConfig}>
             <QueryClientProvider client={queryClient}>
               <WagmiProvider config={config}>
                 <Layout>
