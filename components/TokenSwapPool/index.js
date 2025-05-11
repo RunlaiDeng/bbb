bd1c51eba0090580a762c02f3acb380ed736ce75
Galaxy <30950645+GalaxySciTech@users.noreply.github.com>
2025-05-11 18:20:33 +0400

    Update index.js

--- components/TokenSwapPool/index.js
+++ components/TokenSwapPool/index.js
@@ -450,36 +450,34 @@ const TokenSwap = (props) => {
               )}
             </>
           )}
-          {/* {isBBB && (
-            <div
-              className="text-left font-bold text-xs hover:underline cursor-pointer flex items-center text-red-700"
-              onClick={() => {
-                window.open(xswapDexLink);
-              }}
+          <div
+            className="text-left font-bold text-xs hover:underline cursor-pointer flex items-center text-red-700 mt-2"
+            onClick={() => {
+              window.open(`https://icecreamswap.com/swap?chain=xdc&outputCurrency=${token}`);
+            }}
+          >
+            <svg
+              t="1732767890609"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="1469"
+              width="16"
+              height="16"
             >
-              <svg
-                t="1732767890609"
-                viewBox="0 0 1024 1024"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-                p-id="1469"
-                width="16"
-                height="16"
-              >
-                <path
-                  d="M522.656 388.064a32 32 0 0 0-32 32v160a32 32 0 0 0 64 0v-160a32 32 0 0 0-32-32M522.656 676.064a32 32 0 1 0 0 64 32 32 0 0 0 0-64"
-                  fill="#d81e06"
-                  p-id="1470"
-                ></path>
-                <path
-                  d="M714.656 795.616H203.072l127.584-221.888 33.152-57.664 158.848-276.224 158.816 276.224 33.184 57.696 127.552 221.856h-127.552z m194.528-11.968L566.528 187.712c-10.144-17.6-26.112-27.712-43.872-27.712s-33.728 10.112-43.84 27.712L136.096 783.648c-10.048 17.568-10.784 36.48-1.92 51.84 8.896 15.328 25.6 24.128 45.824 24.128H865.344c20.16 0 36.864-8.8 45.76-24.128 8.896-15.36 8.192-34.24-1.92-51.84z"
-                  fill="#d81e06"
-                  p-id="1471"
-                ></path>
-              </svg>
-              Swap not work ? click to try xswap {"->"}
-            </div>
-          )} */}
+              <path
+                d="M522.656 388.064a32 32 0 0 0-32 32v160a32 32 0 0 0 64 0v-160a32 32 0 0 0-32-32M522.656 676.064a32 32 0 1 0 0 64a32 32 0 0 0 0-64"
+                fill="#d81e06"
+                p-id="1470"
+              ></path>
+              <path
+                d="M714.656 795.616H203.072l127.584-221.888 33.152-57.664 158.848-276.224 158.816 276.224 33.184 57.696 127.552 221.856h-127.552z m194.528-11.968L566.528 187.712c-10.144-17.6-26.112-27.712-43.872-27.712s-33.728 10.112-43.84 27.712L136.096 783.648c-10.048 17.568-10.784 36.48-1.92 51.84 8.896 15.328 25.6 24.128 45.824 24.128H865.344c20.16 0 36.864-8.8 45.76-24.128 8.896-15.36 8.192-34.24-1.92-51.84z"
+                fill="#d81e06"
+                p-id="1471"
+              ></path>
+            </svg>
+            IceCreamSwap API temporary issue. Please trade on icecreamswap.com {"->"} 
+          </div>
         </div>
       </div>
     </>
